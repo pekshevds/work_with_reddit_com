43f60aa023a9d8ace3d5bf2ddc352f1f6d1eb04f
pekshevds <pekshev.ds@gmail.com>
2024-10-02 15:40:52 +0300

    fetching data

--- reddit/__main__.py
+++ reddit/__main__.py
@@ -1,9 +1,10 @@
 """
-Ваша задача – написать скрипт, который спрашивает сабреддит, парсит с него все посты за последние 3 дня и выводит топ пользователей,
-которые написали больше всего комментариев и топ пользователей, которые написали больше всего постов.
+Ваша задача – написать скрипт, который спрашивает сабреддит, парсит с него все посты за последние 3 дня и выводит топ
+пользователей, которые написали больше всего комментариев и топ пользователей, которые написали больше всего постов.
 Топ - это когда сверху тот, кто больше всех написал комментариев/постов, на втором месте следущий за ним и так далее."""
 
 import requests
+from datetime import datetime
 
 
 def user_token() -> str:
@@ -20,5 +21,29 @@ if __name__ == "__main__":
     if not TOKEN:
         raise ValueError("please, update token. python.exe -m reddit.token")
     headers = {"Authorization": f"bearer {TOKEN}"}
-    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-    print(response.json())
+    params = {"limit": "100"}
+
+    full_names = []
+    table = []
+    for _ in range(5):
+        response = requests.get(
+            "https://oauth.reddit.com/r/programming/new", headers=headers, params=params
+        )
+        posts = response.json().get("data").get("children")
+        for post in posts:
+            data = post.get("data")
+            fullname = f"{data.get("kind")}_{data.get("id")}"
+            params["after"] = fullname
+            if fullname not in full_names:
+                table.append(
+                    {
+                        "author": data.get("author"),
+                        "created": datetime.fromtimestamp(
+                            data.get("created_utc")
+                        ).strftime("%Y-%m-%dT%H:%M:%S"),
+                        "num_comments": data.get("num_comments"),
+                    }
+                )
+                full_names.append(fullname)
+    for item in enumerate(table):
+        print(item)
